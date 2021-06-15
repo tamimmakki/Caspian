b730604f4978059da6d289e1402344e39ce4d2bd
Tamim MAKKI <GV5885@engie.com>
2021-06-15 16:41:48 +0100

    02FirstTestPassed

--- CafeCaspian/CafeCaspianTests/Services/MenuService.cs
+++ CafeCaspian/CafeCaspianTests/Services/MenuService.cs
@@ -7,7 +7,42 @@ namespace CafeCaspian.Services
     {
         public IList<MenuItem> GetMenu()
         {
-            throw new NotImplementedException();
+            return new List<MenuItem>
+            {
+                new MenuItem
+                {
+                    Id = 1,
+                    Name = "Cola",
+                    Price = 0.5M,
+                    Category = Category.Drink,
+                    IsHot = false
+
+                },
+                new MenuItem
+                {
+                    Id = 2,
+                    Name = "Coffee",
+                    Price = 1.0M,
+                    Category = Category.Drink,
+                    IsHot = true
+                },
+                new MenuItem
+                {
+                    Id = 3,
+                    Name = "Cheese Sandwich",
+                    Price = 2.0M,
+                    Category = Category.Food,
+                    IsHot = false
+                },
+                new MenuItem
+                {
+                    Id = 4,
+                    Name = "Steak Sandwich",
+                    Price = 4.5M,
+                    Category = Category.Food,
+                    IsHot = true
+                }
+            };
         }
     }
 }
